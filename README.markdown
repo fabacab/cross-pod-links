0a9d0a352e29dfe3ca4c232829c2c1703fb426ca
Meitar Moscovitz <meitarm@gmail.com>
2015-03-10 22:19:28 -0500

    Initial commit.

---
+++ README.markdown
@@ -0,0 +1,38 @@
+# Cross-pod Links (for Diaspora)
+
+The Cross-pod Links (for Diaspora) user script makes it easy to insert permalinks to other Diaspora posts or comments in your own posts or comments that work on all pods. For instance, if Alice has a Diaspora account at Pod-A.com but follows Bob, who has a Diaspora account at Pod-B.com, then when alice follows a link in one of Bob's posts to another post on Diaspora (such as one on Bob's own home pod), she will not be able to interact with it. If Bob uses the convenience functions provided by this user script to create links in his posts, his links will send each person who follows them to the correct version of the post on their own home pod, without Bob having to know ahead of time which pod his contacts are on.
+
+Here's what it looks like:
+
+![Screenshot of Cross-pod Links (for Diaspora) popup with copy to clipboard buttons.](http://i.imgur.com/IuAJD4T.png)
+
+## System requirements
+
+The following software must be installed on your system before installing the user script.
+
+### Google Chrome
+
+If you use the [Google Chrome](https://chrome.google.com/) web browser (version 23 or higher), ensure you have the [Tampermonkey extension](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo) installed.
+
+## Installing
+
+To install this user script, go to [github.com/meitar/cross-pod-links](https://github.com/cross-pod-links/) and click the "[Download and install](https://github.com/meitar/cross-pod-links/raw/master/cross-pod-links.user.js)" near the middle of the page:
+
+> [Download and install Cross-pod Links (for Diaspora)](https://github.com/meitar/cross-pod-links/raw/master/cross-pod-links.user.js)
+
+If you enjoy this script, please consider [donating](http://maybemaimed.com/cyberbusking/) for your use of the script. :) Your donations are sincerely appreciated!
+
+## Using
+
+To use Cross-pod Links (for Diaspora), log in to your Diaspora account and click on the permalink for any post or comment, which is usually small gray text that says something like, "about *some time* ago" near the top or bottom of the post. Clicking on the permalink will reveal a popup that shows the full cross-pod link (a server-relative URL) and two buttons to copy the cross-pod link raw or as a Markdown-formatted hyperlink.
+
+Click the "Copy as Markdown" button to copy the post title and cross-pod link as Markdown. Clicking "Copy to Clipboard" copies only the cross-pod link (the URL), without Markdown formatting. Clicking the "[X]" button closes the popup.
+
+## Contributing
+
+[Patches welcome](https://github.com/meitar/cross-pod-links/issues)!
+
+## Change log
+
+* Version 0.1:
+    * Initial release.
---
+++ cross-pod-links.user.js
@@ -0,0 +1,184 @@
+/**
+ * This is a Greasemonkey script and must be run using a Greasemonkey-compatible browser.
+ */
+// ==UserScript==
+// @name           Cross-pod Links (for Diaspora)
+// @version        0.1
+// @author         maymay <meitar@joindiaspora.com>
+// @namespace      net.maymay.diaspora.cross
+// @updateURL      https://github.com/meitar/cross-pod-links/raw/master/cross-pod-links.user.js
+// @description    Click a Diaspora post's permalink to copy a server-relative URL to your clipboard. Makes it easy to write links in your Diaspora posts that will work across pods when the people you are sharing with are on a different pod than you.
+// @require        https://code.jquery.com/jquery-1.10.2.min.js
+// TODO: Can the include be restricted based on a known podlist?
+// @include        https://*/*
+// @grant          GM_xmlhttpRequest
+// @grant          GM_addStyle
+// @grant          GM_log
+// @grant          GM_setClipboard
+// ==/UserScript==
+
+D_PERMALINKER = {};
+D_PERMALINKER.CONFIG = {
+    'debug': false // switch to true to debug.
+};
+
+// Utility debugging function.
+D_PERMALINKER.log = function (msg) {
+    if (!D_PERMALINKER.CONFIG.debug) { return; }
+    GM_log('CROSS-POD LINKS: ' + msg);
+};
+
+// Initializations.
+GM_addStyle('\
+.cross-pod-links-popup.tooltip.bottom .tooltip-arrow {\
+    left: 35%;\
+}\
+.cross-pod-links-popup .tooltip-inner {\
+    text-align: left;\
+    max-width: 100%;\
+    padding: 10px;\
+}\
+.cross-pod-links-popup button {\
+    float: right;\
+    margin: 0 0 10px 10px;\
+    opacity: 1;\
+    background: #FFF;\
+}\
+.cross-pod-links-popup .tooltip-inner a {\
+    font-size: larger;\
+    display: block;\
+    clear: both;\
+    text-align: center;\
+}\
+.cross-pod-links-popup .tooltip-inner a:hover {\
+    text-decoration: underline;\
+}\
+');
+D_PERMALINKER.init = function () {
+    // Set up a Mutation Observer to listen for new stream contents.
+    var MutationObserver = unsafeWindow.MutationObserver || unsafeWindow.WebKitMutationObserver;
+    var observer = new MutationObserver(function (mutations) {
+        D_PERMALINKER.log('Observing mutations:');
+        mutations.forEach(function (mutation) {
+            var links = mutation.target.querySelectorAll('a[href]');
+            for (var i = 0; i < links.length; i++) {
+                if (links[i].pathname && links[i].pathname.match(/^\/posts\/[0-9a-f]+/)) {
+                    D_PERMALINKER.main(links[i]);
+                }
+            }
+        });
+    });
+    var el = document.getElementById('main_stream')
+        || document.getElementById('container');
+    // TODO: Figure out how to get this working for the notification dropdown, too.
+        //|| document.getElementById('notification_dropdown');
+    try {
+        observer.observe(el, {
+            'childList': true,
+            'subtree': true
+    });
+    } catch (e) {
+        console.log(e);
+    }
+};
+window.addEventListener('DOMContentLoaded', D_PERMALINKER.init);
+
+D_PERMALINKER.setPermalinkGUID = function (anchor_node, guid) {
+    // translate the post URL on this pod
+    // to a post URL for a server-relative cross-pod GUID
+    var new_url = '/posts/' + guid;
+    if (anchor_node.search) {
+        new_url += anchor_node.search;
+    }
+    if (anchor_node.hash) {
+        new_url += anchor_node.hash;
+    }
+    anchor_node.setAttribute('href', new_url);
+};
+
+D_PERMALINKER.getCrossPodUrl = function (anchor_node) {
+    //https://joindiaspora.com/posts/1521e8a0a9bd0132a7710e379eef12a8#81d30200a9bf0132a7710e379eef12a8
+    var new_url = anchor_node.pathname;
+    if (anchor_node.search) {
+        new_url += anchor_node.search;
+    }
+    if (anchor_node.hash) {
+        new_url += anchor_node.hash;
+    }
+    return new_url;
+};
+
+D_PERMALINKER.getPostRoot = function (node) {
+    var el = jQuery(node).closest('.stream_element');
+    if (0 === el.length) {
+        el = jQuery(node).closest('#single-post-container');
+    }
+    if (undefined === el.get(0)) {
+        D_PERMALINKER.log('No post root element found for node ' + node);
+        return false;
+    } else {
+        return el.get(0);
+    }
+}
+
+D_PERMALINKER.setDataAttributes = function (anchor_node, data) {
+    el = jQuery(D_PERMALINKER.getPostRoot(anchor_node));
+    el.attr('data-post-title', data.title); // Why won't .data() work here?
+};
+
+D_PERMALINKER.updatePermalink = function (anchor_node) {
+    var url = window.location.protocol + '//' + window.location.host + anchor_node.pathname + '.json';
+    GM_xmlhttpRequest({
+        'method': 'GET',
+        'url': url,
+        'onload': function (response) {
+            try {
+                var json = JSON.parse(response.responseText);
+            } catch (e) {
+                D_PERMALINKER.log('Error parsing received JSON from ' + url);
+                D_PERMALINKER.log(response.responseText);
+            }
+            D_PERMALINKER.setPermalinkGUID(anchor_node, json.guid);
+            D_PERMALINKER.setDataAttributes(anchor_node, json);
+        }
+    });
+};
+
+D_PERMALINKER.createSimplePopUp = function (node) {
+    var div = document.createElement('div');
+    div.setAttribute('class', 'cross-pod-links-popup tooltip fade bottom in');
+    var html = '';
+    html += '<div class="tooltip-arrow"></div>';
+    html += '<div class="tooltip-inner">';
+    html += '<button class="close" onclick="this.parentNode.parentNode.parentNode.removeChild(document.querySelector(\'.cross-pod-links-popup\'));">[X]</button>';
+    html += '<button class="cross-pod-links-pbcopy">Copy to Clipboard</button>';
+    html += '<button class="cross-pod-links-pbcopy-markdown">Copy as Markdown</button>';
+    html += '<a href="' + D_PERMALINKER.getCrossPodUrl(node) + '">';
+    html += 'Cross-pod Permalink: ' + D_PERMALINKER.getCrossPodUrl(node);
+    html += '</a>';
+    html += '</div>';
+    div.innerHTML = html;
+    jQuery(div.querySelector('button.cross-pod-links-pbcopy')).on('click', function (e) {
+        GM_setClipboard(D_PERMALINKER.getCrossPodUrl(node));
+    });
+    jQuery(div.querySelector('button.cross-pod-links-pbcopy-markdown')).on('click', function (e) {
+        GM_setClipboard('[' + D_PERMALINKER.getPostRoot(node).getAttribute('data-post-title') + '](' + D_PERMALINKER.getCrossPodUrl(node) + ')');
+    });
+    node.parentNode.appendChild(div);
+};
+
+// This is the main() function, executed on page load.
+D_PERMALINKER.main = function (anchor_node) {
+    D_PERMALINKER.updatePermalink(anchor_node);
+
+    // attach a click listener on this permalink anchor element
+    if (!jQuery(anchor_node).closest('.cross-pod-links-popup').length) {
+        jQuery(anchor_node).unbind('click'); // not sure what was previously bound?
+        jQuery(anchor_node).on('click', function (e) {
+            e.preventDefault();
+            e.stopPropagation();
+            // display a small "tooltip" popup over the link
+            D_PERMALINKER.createSimplePopUp(anchor_node);
+        });
+    }
+};
